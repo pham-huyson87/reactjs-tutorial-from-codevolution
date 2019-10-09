45546585956e6b26d8d56e6beba0945d225d7166
pham-huyson87 <pham.huyson87@hotmail.com>
2019-10-08 20:08:08 -0400

    - Add Styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import './App.css';
 import FunctionComponent from './components/FunctionComponent';
 import ClassComponent from './components/ClassComponent';
 import FormHandling from './components/FormHandling';
+import Stylesheet from './components/Stylesheet';
+import Inline from './components/Inline';
+import './appStyles.css';
+import styles from './appStyles.module.css';
+import Fragment from './components/Fragment';
 
 
 function App() {
@@ -14,6 +19,13 @@ function App() {
       <ClassComponent />
       <FormHandling />
       <Fragment />
+      
+      <Stylesheet primary />
+      <Inline />
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1>
+
+    
     </div>
   );
 }
